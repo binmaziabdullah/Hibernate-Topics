d8ab83a391ea1e486d40721552caef046c879f68
abdullah <binmazi22@gmail.com>
2021-05-18 18:12:06 +0530

    Initial commit

---
+++ src/main/java/com/hibernate/concept/hibernatetopics/HibernateTopicsApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate.concept.hibernatetopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateTopicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibernate/concept/hibernatetopics/HibernateTopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate.concept.hibernatetopics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateTopicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
